f96beee6db11585a755ac249e0390ab28aac47d1
Maria Smith <142597328+Mithsd@users.noreply.github.com>
2023-08-27 23:25:06 -0300

    Add files via upload

---
+++ TAREFA_13.js
@@ -0,0 +1,9 @@
+
+ let tabuada = 10;
+    let resultado;
+
+    for(let i = 1; i <= 10; i++)
+    {
+        resultado = i * tabuada;
+        console.log(`${i} x ${tabuada} = ${resultado}`);
+    }

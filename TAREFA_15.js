eb3f6ef9dc01e929f0fc6feb8dcd10c857e999e6
Maria Smith <142597328+Mithsd@users.noreply.github.com>
2023-08-27 23:31:06 -0300

    Add files via upload

---
+++ TAREFA_15.js
@@ -0,0 +1,6 @@
+let filme = {
+        titulo: "ACROSS THE SPIDERVERSE",
+        nota: 10,
+        genero: "animação"
+    }
+    console.log(`Título: ${filme.titulo}\nGênero: ${filme.genero}\nNota: ${filme.nota}`);

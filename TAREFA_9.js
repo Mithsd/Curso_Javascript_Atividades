b2bb78253446b69b3b3b1a4a3546101846fa0fe1
Maria Smith <142597328+Mithsd@users.noreply.github.com>
2023-08-27 23:10:03 -0300

    Add files via upload

---
+++ TAREFA_9.js
@@ -0,0 +1,12 @@
+
+    let ano=2018;
+    let total=2023-ano;
+
+    if (total>=18) 
+    {
+        console.log(`pode votar`);  
+    }
+    else
+    {
+        console.log(`nao pode votar`); 
+    }

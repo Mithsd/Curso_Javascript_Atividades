2eb586f2631c778080a6b1db8849a52ff8e27890
Maria Smith <142597328+Mithsd@users.noreply.github.com>
2023-08-27 23:28:54 -0300

    Add files via upload

---
+++ TAREFA_14.js
@@ -0,0 +1,11 @@
+
+ let numeros = [1,2,3];
+    let result;
+
+    for(let i = 1; i <= 10; i++)
+    {
+        result = result + numeros[i];
+        
+    }
+    let media = result/3;
+    console.log(result);
